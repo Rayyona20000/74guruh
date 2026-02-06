147b2763c3550d4fe901199b3b6a1a7d5d5d129f
bahridinulugbekof-ai <bahridinulugbekof@gmail.com>
2026-02-06 15:56:00 +0500

    MA

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const App = () => {
   return (
     <div className='w-"1200px" m-auto border-amber-500  ' >
-<div>
+<div className='w-"200px"'>
   abdushkur
 </div>
 
-<div>
+<div className='w-"600px"'>
   baxriddin
 </div>
 
-<div>
+<div className='w-"400px"'>
 rayyona
 
 </div>
