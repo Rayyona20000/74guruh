f89cdd3a1882354d28e45703103458357263acd0
function339-beep <functionmark339@gmail.com>
2026-02-06 10:21:08 +0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,21 @@ import React from 'react'
 const App = () => {
   return (
     <div className='w-"1200px" m-auto border-4 border-l-emerald-50  ' >
-<div className='w-"200px"'>
-  abdushkur
+<div className='navbar w-[1200px] h-[100px]'>
+  <h1>SPECULAR</h1>
+    <div className="box1 w-[70%]">
+      <h1>home</h1>
+      <h1>demos</h1>
+      <h1>pages</h1>
+      <h1>portfolid</h1>
+      <h1>blog</h1>
+      <h1>elements</h1>
+      <h1>shop</h1>
+      <FontAwesomeIcon className='color-[white]' icon={byPrefixAndName.fas['magnifying-glass']} />
+      <FontAwesomeIcon className='color-[white]' icon={byPrefixAndName.fas['cart-shopping']} />
+      <FontAwesomeIcon className='color-[white]' icon={byPrefixAndName.fas['bars']} />
+
+    </div>
 </div>
 
 <div className='w-"600px"'>
